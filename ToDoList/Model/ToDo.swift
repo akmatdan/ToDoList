8b88558c8087429bfe1ec08797b9c6550ecf8c53
Dan Akmat <akmatdan@gmail.com>
2022-10-17 09:34:43 +0600

    Created Model
    ToDo struct

---
+++ ToDoList/Model/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Daniil Akmatov on 17/10/22.
+//
+
+import Foundation
+
+struct ToDo {
+    let title: String
+    let discription: String
+    let isComplete: Bool = false
+}
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let todos = [
+        ToDo(title: "Make vanilla pudding.", discription: "Watch youtube video and follow guidance."),
+        ToDo(title: "Put pudding in a mayo jarl.", discription: "lore ipsume..."),
+        ToDo(title: "Eat it in a public place.", discription: "Mmm..."),
+        
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
