682045b30492bdc1639aa78308ce848126d05f23
Dan Akmat <akmatdan@gmail.com>
2022-10-17 11:33:05 +0600

    TableViewCells
    Req. methods & Index Path for TableViewCells

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -11,15 +11,26 @@ class ViewController: UIViewController {
 
     let todos = [
         ToDo(title: "Make vanilla pudding.", discription: "Watch youtube video and follow guidance."),
-        ToDo(title: "Put pudding in a mayo jarl.", discription: "lore ipsume..."),
+        ToDo(title: "Put pudding in a mayo jar.", discription: "lore ipsume..."),
         ToDo(title: "Eat it in a public place.", discription: "Mmm..."),
-        
     ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let todo = todos[indexPath.row]
+        let cell = UITableViewCell()
+        cell.textLabel?.text = todo.title
+        return cell
+    }
+}
